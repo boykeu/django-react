6598a1feac35a5b2687295a895cda43cd99f637a
boykeu <muhammad.boyke@sci.ui.ac.id>
2021-02-08 17:20:53 +0700

    first cum

---
+++ schema.py
@@ -0,0 +1,29 @@
+import graphene
+import json
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+    is_admin = graphene.Boolean()
+
+    def resolve_hello(self, info):
+        return "world"
+
+    def resolve_is_admin(self, info):
+        return True
+
+
+schema = graphene.Schema(query=Query, auto_camelcase=False)
+
+result = schema.execute(
+    '''
+    {
+        is_admin
+    }
+    '''
+)
+
+# print(result.data['hello'])
+dictResult = dict(result.data.items())
+
+print(json.dumps(dictResult, indent=2))
